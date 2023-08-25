96778cad05877f82f9f340c2f823ce475cd15a0a
eu-neita <neitass.music@gmail.com>
2023-08-25 20:21:18 -0300

    feat: create matches

---
+++ app/backend/src/database/migrations/02-create-matches.ts
@@ -0,0 +1,38 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+import { IMatches } from '../../Interfaces/IMatches';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IMatches>>('matches', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      homeTeamId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      homeTeamGoals: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      awayTeamId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      awayTeamGoals: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      inProgress: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+    });
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('matches');
+  },
+};
