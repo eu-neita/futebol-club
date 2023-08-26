e4df878f0fc0eab2e8e48a6370e6f06733698a52
eu-neita <neitass.music@gmail.com>
2023-08-26 17:13:25 -0300

    fix: testes imports

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,5 +1,6 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import 'mocha';
 import teamMock from './teamsMock'
 // @ts-ignore
 import chaiHttp = require('chai-http');
@@ -8,7 +9,6 @@ import { app } from '../app';
 import Team from '../database/models/TeamModel';
 
 import { Response } from 'superagent';
-import { after, before } from 'node:test';
 
 chai.use(chaiHttp);
 
@@ -27,19 +27,30 @@ describe('/teams', () => {
       .resolves(teamMock as Team);
   });
 
+
+  it('should create a team', async () => {
+    const team = await Team.create({
+      teamName: 'Test Team',
+    });
+    expect(team).to.have.property('id');
+    expect(team).to.have.property('teamName', 'Test Team');
+  });
+
+  it('should return all teams with status 200', async () => {
+    const res = await chai.request(app).get('/teams');
+    expect(res).to.have.status(200);
+    expect(res.body).to.be.an('array');
+    expect(res.body).to.have.length.greaterThan(0);
+    expect(res.body[0]).to.have.property('id');
+    expect(res.body[0]).to.have.property('teamName');
+  });
+
+
   after(()=>{
     (Team.findOne as sinon.SinonStub).restore();
   })
 
-  describe('/teams endpoint', () => {
-    it('should return all teams with status 200', async () => {
-      const res = await chai.request(app).get('/teams');
-      expect(res).to.have.status(200);
-      expect(res.body).to.be.an('array');
-      expect(res.body).to.have.length.greaterThan(0);
-      expect(res.body[0]).to.have.property('id');
-      expect(res.body[0]).to.have.property('teamName');
-    });
+  it('model test', async () => {
   });
 
   it('Seu sub-teste', () => {
