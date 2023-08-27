5e9312f13bc8292db130c751172325fd5cf83e58
eu-neita <neitass.music@gmail.com>
2023-08-27 14:57:14 -0300

    feat: tests route /teams/:id

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,7 +1,7 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 import 'mocha';
-import teamMock from './teamsMock'
+import teamMock from './mocks/teamsMock'
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
@@ -15,10 +15,6 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('/teams', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
-
   let chaiHttpResponse: Response;
 
   before(async () => {
@@ -28,7 +24,7 @@ describe('/teams', () => {
   });
 
 
-  it('should create a team', async () => {
+  it('should create a team table', async () => {
     const team = await Team.create({
       teamName: 'Test Team',
     });
@@ -45,15 +41,34 @@ describe('/teams', () => {
     expect(res.body[0]).to.have.property('teamName');
   });
 
+  after(()=>{
+    (Team.findOne as sinon.SinonStub).restore();
+  })
+
+});
+
+describe('/teams:id', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(Team, "findOne")
+      .resolves(teamMock as Team);
+  });
+
+  it('should return all teams with status 200', async () => {
+    const res = await chai.request(app).get('/teams/5');
+    expect(res).to.have.status(200);
+    // expect(res.body).to.be.an('array');
+    expect(res.body).to.have.length.greaterThan(0);
+    expect(res.body).to.have.property('id');
+    expect(res.body.message.id).to.been.deep.equal('5');
+    expect(res.body).to.have.property('teamName');
+    expect(res.body.message.teamName).to.been.deep.equal('Cruzeiro');
+  });
 
   after(()=>{
     (Team.findOne as sinon.SinonStub).restore();
   })
 
-  it('model test', async () => {
-  });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
 });
