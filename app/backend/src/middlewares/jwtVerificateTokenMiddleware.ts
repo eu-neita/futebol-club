6e16d9042aa7c8fad26099a9b353fc38b33729be
eu-neita <neitass.music@gmail.com>
2023-08-27 20:40:26 -0300

    fix: remove barrer func

--- app/backend/src/middlewares/jwtVerificateTokenMiddleware.ts
+++ app/backend/src/middlewares/jwtVerificateTokenMiddleware.ts
@@ -1,14 +1,15 @@
 import * as jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 
-const jwtVerificated = async (req: Request, res: Response, next: NextFunction) => {
-  const { authorization } = req.headers;
+const removeBarrer = (auth: string | undefined) => auth?.split(' ')[1] || '';
 
+const jwtVerificated = (req: Request, res: Response, next: NextFunction) => {
+  const { authorization } = req.headers;
   if (!authorization) {
     return res.status(401).json({ message: 'Token not found' });
   }
 
-  const decoded = jwt.decode(authorization) as jwt.JwtPayload | null;
+  const decoded = jwt.decode(removeBarrer(authorization)) as jwt.JwtPayload | null;
 
   if (!decoded?.email) {
     return res.status(401).json({ message: 'Token must be a valid token' });
